77ae3546267718a807a96008d76344b4d659e65c
akshatcoder-hash <akshatcoder@outlook.com>
2023-04-06 02:49:05 +0530

    Playing with memory

---
+++ mem.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+
+int main(int argc, char *argv[]) {
+    int *p; 
+    p = malloc(sizeof(int));
+    assert(p != NULL);
+    printf("(%d) addr pointed to by p: %p\n", (int) getpid(), p);
+    *p = 0; 
+    while (1) {
+	Spin(1);
+	*p = *p + 1;
+	printf("(%d) p: %d\n", getpid(), *p);
+    }
+    return 0;
+}
